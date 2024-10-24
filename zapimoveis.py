68d0cf5bdc2fa8ae9cc161ba87ced6ec7ef3aae8
Nelson Seixas <nsouzarj@outlook.com>
2024-10-24 16:04:53 -0300

    Feito

--- zapimoveis.py
+++ zapimoveis.py
@@ -32,20 +32,24 @@ tipo_de_filtro=sys.argv[4]
 
 # Configurando opções do Chrome
 chrome_options = Options()
+chrome_options.page_load_strategy="normal"
+chrome_options.set_capability('goog:logginPrefs',{'performance': 'ALL'})
 chrome_options.add_argument("--headless")  # Comente esta linha para ver o navegador
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--incognito")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")  # Desativa a aceleração de hardware
+chrome_options.add_argument("--disable-extensions") 
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-requests_cache.install_cache('cache')
+#requests_cache.install_cache('cache')
 # Inicializando o ChromeDriver
 # Caminho para o ChromeDriver
 chrome_driver_path = caminho_do_webchrome
 #"C:\Users\User\ChromeWithDriver\chromedriver.exe"
 # Configurar o cache (use 'cache' como nome do arquivo do cache)
 
-service = Service(chrome_driver_path)
-driver = webdriver.Chrome(service=service, options= chrome_options, keep_alive=True)
+#service = Service(chrome_driver_path)
+#driver = webdriver.Chrome(service=service, options= chrome_options, keep_alive=True)
 
 
 # Inicializando um conjunto para armazenar links únicos
@@ -68,15 +72,17 @@ contador_geral = 1;
 #actions.move_to_eleme
 
 
+registro = 0   
 
 # nt(drawer)  # Move the mouse to the drawer initiall
 # Loop para navegar pelas páginas
+
 try:
     while pagina_atual >= 1:
-
+         
         service = Service(chrome_driver_path)
-        driver = webdriver.Chrome(service=service, options=chrome_options)# Loop principal para as páginas
-        registro = 0   
+        driver = webdriver.Chrome(service=service, options=chrome_options,keep_alive=True)#
+        
          
         # Montando a URL com o número da página atual
         link_nova_lina = caminho_do_filtro + str(pagina_atual)
@@ -92,10 +98,11 @@ try:
         # O resto do seu código (dentro do loop) continua exatamente igual:
         driver.maximize_window()
         driver.get(url)
-        actions = ActionChains(driver)
-
         print("COLENTANDO OS LINK DOS IMOvES..")
         print((f"PAGINA ATUAL: {pagina_atual} "))
+        driver.maximize_window()
+        #actions = ActionChains(driver) 
+       
 
         # Espera até que os imóveis estejam carregados
         # WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/main/section/div/form/div[2]/div[4]/div[1]/div/div[1]/div/a/div/div[1]/div[2]')))
@@ -109,23 +116,32 @@ try:
         if len(imoveis) == 1:
             print("Final de leitura dos registros.")
             break 
-
+        
         # Pagina os registro em cadas pagina ate 106 no maximo
         a = 0
         print(f"PAGINANDO REGISTROS: {pagina_atual}")
-        driver.maximize_window()
-        actions = ActionChains(driver)
+        
         # Loop para iterar pelos elementos de imóvel
         for cont in range(1, 150):  # Limita a 105 registros
             try:
                 # Coleta o link e outros dados
                 a += 1
+                if a == 14:
+                     driver.execute_script("arguments[0].scrollIntoView();", imoveis[-1])
+                    # time.sleep(2) 
                    
+                   #Ajuste o tempo de espera conforme necessário
+                     a = 0
+                     a += 1
+                   
+                driver.implicitly_wait(0)   
                 link_element = driver.find_elements(By.XPATH, f'//*[@id="__next"]/main/section/div/form/div[2]/div[4]/div[1]/div/div[{cont}]/div/a')
+                
+                botao_menssagen = driver.find_elements(By.XPATH, f'//*[@id="__next"]/main/section/div/form/div[2]/div[4]/div[1]/div/div[{cont}]/div/a/div/div/div[2]/div[3]/div[2]/div[1]/button[2]')  
                   
                 botao_anuncio = driver.find_elements(By.XPATH, f'//*[@id="__next"]/main/section/div/form/div[2]/div[4]/div[1]/div/div[{cont}]/div/div/div/div/div[2]/div[3]/div[2]/button')   
                
-                botao_menssagen = driver.find_elements(By.XPATH, f'//*[@id="__next"]/main/section/div/form/div[2]/div[4]/div[1]/div/div[{cont}]/div/a/div/div/div[2]/div[3]/div[2]/div[1]/button[2]')  
+              
  
                 if botao_anuncio and len(botao_anuncio) > 0 and len(botao_menssagen)==0: 
                     # Encontra o botão clicável usando WebDriverWait
@@ -134,7 +150,6 @@ try:
                     
                     # Clica no botão de anúncio
                     try:
-                       driver.implicitly_wait(5) 
                        driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.ESCAPE) # Fechando o anúnci
                        button1.click()
                        time.sleep(5)
@@ -151,7 +166,7 @@ try:
                         links_imoveis.add(link_result)
                         
                         driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.ESCAPE) # Fechando o anúncio
-                        time.sleep(4)
+                        
 
                 # Coleta o link do imóvel (se não houver anúncio)
                 if link_element and len(link_element) > 0 and len(botao_menssagen) > 0:
@@ -160,20 +175,19 @@ try:
                     print(f"LINK: {cont} - {link}")
 
                 # Rola a página para carregar mais imóveis
-                if a == 14:
-                    driver.execute_script("arguments[0].scrollIntoView();", imoveis[-1])
-                    time.sleep(2)  # Ajuste o tempo de espera conforme necessário
-                    a = 0
-                
+            
+              
                 if cont ==105:
                     break# Não use 'continue' aqui, pois ele está fora do bloco 'try'
 
             except Exception as e:  # Captura qualquer exceção
-                #print(f"ERRO: {e}")
+                print(f"ERRO: {e}")
                 continue  # Continua para a próxima iteração do loop
             
             # Continua para o próximo registro
+      
         pagina_atual+=1
+        
                 
 
 
@@ -233,29 +247,30 @@ sheet.append(cabecalhos)  # Adiciona os cabeçalhos à planilha
 registro_atual = 0
 for link in links_imoveis:
     # Inicializando o ChromeDriver
-    service1 = Service(chrome_driver_path)
-    driver1 = webdriver.Chrome(service=service1, options=chrome_options,keep_alive=True)
+  
     try:
-      wait = WebDriverWait(driver1, 5)
+      service1 = Service(chrome_driver_path)
+      driver1 = webdriver.Chrome(service=service1, options=chrome_options,keep_alive=True)
+   
+     
       driver1.get(link) # Acessa a página do imóvel
+      driver1.implicitly_wait(1) 
       print(f"LINK - {link}")
     except Exception as e:
       print(f"ERROR: Na leirura do endereço ---> {e} ---> {link}")
       break
     
     try:
-        
+       
        
         
-        items_lista=WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[1]/div[4]/div/div/div')))
+       #items_lista= driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[1]/div[4]/div/div/div') #WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[1]/div[4]/div/div/div')))
        # Aguarda a presença do elemento com o XPath
         """ Traz os items para ser tartado"""
-        items_lista = WebDriverWait(driver1, 10).until(
-            EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[1]'))
-                                                      
-        )
+
+        items_lista =  WebDriverWait(driver1, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[1]'))                                                )
         # Traz os valores do imovel
-        precos_imovel = WebDriverWait(driver1, 10).until(
+        precos_imovel = WebDriverWait(driver1, 5).until(
             EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]'))
                                                       
         )
@@ -298,22 +313,22 @@ for link in links_imoveis:
              property_value = item.find('span', class_='amenities-item-text').text.strip()        
              num_vagas = extrair_numeros(property_value)           
    
-        title = WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[2]/section/div[1]/h1')))
-        address = WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[1]/div[5]/div[1]/p')))
+        title = WebDriverWait(driver1, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[2]/section/div[1]/h1')))
+        address = WebDriverWait(driver1, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[1]/div[5]/div[1]/p')))
      
         # PEga tipo de valor "vnda" "aliguel"
-        tipo_valor_venda=WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="business-type-info"]')))   
+        tipo_valor_venda=WebDriverWait(driver1, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="business-type-info"]')))   
         if tipo_valor_venda.text == "Venda":
-            valor_venda = WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.XPATH, '//p[@data-testid="price-info-value"]')))    
+            valor_venda = WebDriverWait(driver1, 5).until(EC.presence_of_element_located((By.XPATH, '//p[@data-testid="price-info-value"]')))    
             valor_venda_limpo=valor_venda.text;
             valor_aluguel_limpo=""
             #valor_venda = WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.XPATH, ' /html/body/div[2]/div[1]/div[1]/div[1]/div[3]/div/div[1]/div[1]/p[2]')))
             if valor_venda.text != "Sob consulta":
            
-                condo_fee = WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="condo-fee-price"]')))
+                condo_fee = WebDriverWait(driver1,5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="condo-fee-price"]')))
                 if not condo_fee.text:
                    condo_fee = "Isento"  # Assinala um valor padrão se vazio
-                iptu = WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="iptu-price"]')))
+                iptu = WebDriverWait(driver1, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="iptu-price"]')))
                 if not iptu.text:
                    iptu = "Isento"  # Assinala um valor padrão se vazi  
                
@@ -329,15 +344,15 @@ for link in links_imoveis:
                     
         elif tipo_valor_venda.text == "Aluguel":
                                                                                                        
-            valor_aluguel = WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[1]/div[3]/div/div[1]/div[1]/p[2]')))
+            valor_aluguel = WebDriverWait(driver1, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[1]/div[3]/div/div[1]/div[1]/p[2]')))
             valor_aluguel_limpo=valor_aluguel.text
             valor_venda_limpo=""
             
-            condo_fee = WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="condo-fee-price"]')))
+            condo_fee = WebDriverWait(driver1, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="condo-fee-price"]')))
             if not condo_fee.text:
                condo_fee = "Isento"  # Assinala um valor padrão se vazio
             
-            iptu = WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="iptu-price"]')))
+            iptu = WebDriverWait(driver1, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="iptu-price"]')))
             if not iptu.text:
                iptu = "Isento"  # Assinala um valor padrão se vazio
             
@@ -352,25 +367,25 @@ for link in links_imoveis:
             else:
                iptu = iptu.text   
         
-            area_valores=   WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]')))
+            area_valores=   WebDriverWait(driver1, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]')))
             if not area_valores.text:
                 print("Nao existe")
                 
         # Traz a data em extendo e trasnforma em formato MM/DD/YYYy
-        data_extenso = WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[2]/section/div[4]/span[2]')))  
+        data_extenso = WebDriverWait(driver1, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[2]/section/div[4]/span[2]')))  
         data_cadastro = extrair_e_formatar_data(data_extenso.text)
         #area_num = remover_parte_texto(area.text, "m²")
         # Click the button at the specified XPath
         
            
-        button2 = WebDriverWait(driver1, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[2]/section/div[3]/button')))
+        button2 = WebDriverWait(driver1,5).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[2]/section/div[3]/button')))
         if button2:
            button2.click()  
            time.sleep(3)
         # e do imovel 
-        anunciante_do_imovel = WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div[2]/div/section/section/div[1]/div/p[1]')))
+        anunciante_do_imovel = WebDriverWait(driver1, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div[2]/div/section/section/div[1]/div/p[1]')))
         # Zap 
-        zap_do_anunciante = WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div[2]/div/section/section/div[2]/p[3]')))
+        zap_do_anunciante = WebDriverWait(driver1, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div[2]/div/section/section/div[2]/p[3]')))
         # Imprime na tela o link   
         logradouro, numero, bairro, cidade, estado = parse_address(address.text)
            
